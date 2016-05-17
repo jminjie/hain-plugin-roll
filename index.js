33899979962c80c3fce2250be45531e4ce67f9ab
Jordan <jminjie@umich.edu>
2016-05-17 17:04:16 +0000

    Fix attribution

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * d20.js
+ * Taken from d20.js
  *
  * Javascript library for rolling dice. Supports strings written in a dice-rolling syntax, eg. "d20", "4d6", "1d8+1".
  *
@@ -19,52 +19,57 @@ function d20roll(dice) {
             return num;
 }
 
-	function verboseRoll(dice) {
-		var amount = 1,
-			mod = 0,
-			results = [],
-			match,
-			num,
-			modifiers;
+function verboseRoll(dice) {
+    var amount = 1,
+        mod = 0,
+        results = [],
+        match,
+        num,
+        modifiers;
 
-		if (!dice) {
-			throw new Error('Missing dice parameter.');
-		}
+    if (!dice) {
+        throw new Error('Missing dice parameter.');
+    }
 
-		if (typeof dice == 'string') {
-			match = dice.match(/^\s*(\d+)?\s*d\s*(\d+)\s*(.*?)\s*$/);
-			if (match) {
-				if (match[1]) {
-					amount = parseInt(match[1]);
-				}
-				if (match[2]) {
-					dice = parseInt(match[2]);
-				}
-				if (match[3]) {
-					modifiers = match[3].match(/([+-]\s*\d+)/g);
-					for (var i = 0; i < modifiers.length; i++) {
-						mod += parseInt(modifiers[i].replace(/\s/g, ''));
-					}
-				}
-			} else {
-				parseInt(dice);
-			}
-		}
+    if (typeof dice == 'string') {
+        match = dice.match(/^\s*(\d+)?\s*d\s*(\d+)\s*(.*?)\s*$/);
+        if (match) {
+            if (match[1]) {
+                amount = parseInt(match[1]);
+            }
+            if (match[2]) {
+                dice = parseInt(match[2]);
+            }
+            if (match[3]) {
+                modifiers = match[3].match(/([+-]\s*\d+)/g);
+                for (var i = 0; i < modifiers.length; i++) {
+                    mod += parseInt(modifiers[i].replace(/\s/g, ''));
+                }
+            }
+        } else {
+            parseInt(dice);
+        }
+    }
 
-		for (var i = 0; i < amount; i++) {
-			num = Math.floor(Math.random() * dice + 1);
-			results.push(num);
-		}
+    for (var i = 0; i < amount; i++) {
+        num = Math.floor(Math.random() * dice + 1);
+        results.push(num);
+    }
 
-		results = results.sort(function(a, b) {
-			return a - b;
-		});
-		if (mod != 0) {
-			results.push(mod);
-		}
+    results = results.sort(function(a, b) {
+        return a - b;
+    });
+    if (mod != 0) {
+        results.push(mod);
+    }
 
-		return results;
-	}
+    return results;
+}
+
+/* Define the Hain plugin
+ * @author Jordan Liu
+ * @license MIT
+ */
 
 module.exports = (pluginContext) => {
     const logger = pluginContext.logger;
